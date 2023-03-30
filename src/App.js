724a8c78207c93c2ddb34543e02d4d20635c0c1e
syalikh <syalikhfadin@gmail.com>
2023-03-30 14:50:18 +0700

    Percobaan pertama

--- src/App.js
+++ src/App.js
@@ -1,27 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <br />
-          OKE DONE YAAAAKKKKK
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Timer extends Component {
+      constructor(props){
+        super(props)
+        this.state = {
+          time : props.start
+        }
+      }
+
+      componentDidMount(){
+        this.addInterval = setInterval( () => this.increase(), 1000);
+      }
+
+      componentWillUnmount(){
+        clearInterval(this.addInterval)
+      }
+
+
+      increase() {
+        this.setState((state, props) => ({
+          time : parseInt(state.time) +1
+        }))
+      }
+
+
+    render() {
+      return (
+        <div> {this.state.time} Detik </div>
+      )
+    }
+
 }
 
-export default App;
+function Mentor(props){
+  return <span> its {props.age} </span>
+}
+
+function Greeting(props){
+  return <h1> Halo {props.name} - <Mentor age={props.age} /></h1>
+}
+
+
+    class app extends Component {
+      render() {  
+        return (
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <Greeting name= 'Syalikh fadin' age='My first react' />
+              <Timer start="0" />
+            </header>
+          </div>
+        );
+      }
+    }
+
+    export default app;
+  
+
+
